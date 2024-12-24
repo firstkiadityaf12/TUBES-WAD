99c7ba2f4d98e24a70ec00d7d137eba59fd967bf
firstkiaditya <adityafernanda2013@gmail.com>
2024-12-24 14:27:09 +0700

    feature: setup model databse

---
+++ database/migrations/2024_12_24_071206_create_pemasukan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pemasukans', function (Blueprint $table) {
+            $table->id();
+            $table->string('tanggal_pemasukan');
+            $table->string('sumber_pemasukan');
+            $table->float('jumlah_pemasukan');
+            $table->string('deskripsi');
+            $table->integer('id_akun_bank');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pemasukans');
+    }
+};

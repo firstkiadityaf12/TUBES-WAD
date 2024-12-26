87ba223473655a2394963cb381ed5466c7b8e8bd
Dava Ihza Bagus S <dava.ihza18@gmail.com>
2024-12-26 19:50:02 +0700

    backup route

--- routes/web.php
+++ routes/web.php
@@ -47,13 +47,9 @@ Route::post('tagihan/{tagihan}/bayar', [TagihanController::class, 'pay'])->name(
 
 // Routing API Laporan
 Route::resource('laporan_keuangan', LaporanKeuanganController::class);
-<<<<<<< HEAD
-Route::get('laporan_keuangan/{laporan}/transaksi', [LaporanKeuanganController::class, 'showTransactions'])->name('laporan-keuangan.transactions');
-Route::get('laporan_keuangan/filter/{periode}', [LaporanKeuanganController::class, 'filterByPeriod'])->name('laporan-keuangan.filter');
-Route::get('laporan_keuangan/export', [LaporanKeuanganController::class, 'export'])->name('laporan-keuangan.export');
-=======
+
 Route::get('laporan_keuangan/{laporan}/transaksi', [LaporanKeuanganController::class, 'showTransactions'])->name('laporan_keuangan.transactions');
 Route::get('laporan_keuangan/filter/{periode}', [LaporanKeuanganController::class, 'filterByPeriod'])->name('laporan_keuangan.filter');
 Route::get('laporan_keuangan/export', [LaporanKeuanganController::class, 'export'])->name('laporan_keuangan.export');
-Route::get('laporan_keuangan/create', [LaporanKeuanganController::class, 'create'])->name('laporan_keuangan.create');
->>>>>>> laporan
+
+

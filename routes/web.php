0e03d0d4b98c488258e3599cf5bd9d1ccacca855
Dimas Ibnu Muthi <dimasibnumuthi@gmail.com>
2024-12-26 15:16:20 +0700

    #2 Routing untuk Laporan

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LaporanKeuanganController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::resource('laporan_keuangan', LaporanKeuanganController::class);
+Route::get('laporan_keuangan/{laporan}/transaksi', [LaporanKeuanganController::class, 'showTransactions'])->name('laporan_keuangan.transactions');
+Route::get('laporan_keuangan/filter/{periode}', [LaporanKeuanganController::class, 'filterByPeriod'])->name('laporan_keuangan.filter');
+Route::get('laporan_keuangan/export', [LaporanKeuanganController::class, 'export'])->name('laporan_keuangan.export');
